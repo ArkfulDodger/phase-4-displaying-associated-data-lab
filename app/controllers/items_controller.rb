c021722e0ca0bf61629eab2e084779ba51278021
Noah Reece <noah.w.reece@gmail.com>
2022-04-27 22:24:31 -0400

    commit tree 3be02a2953cb59b8e6c273c5144988472c008ea6

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,9 @@
 class ItemsController < ApplicationController
+  # GET /item
+  def index
+    items = Item.all
+    render json: items,
+           only: %i[id name description price],
+           include: [user: { only: %i[id username city] }]
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: %i[show]
+
+  # GET /users/:id
+  def show
+    render json: @user,
+           only: %i[id username city],
+           include: [items: { only: %i[id name description price] }]
+  end
+
+  private
+
+  # set instance variable for show/update/destroy
+  def find_user
+    @user = User.find(params[:id])
+  end
 end
